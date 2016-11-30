0e809537e448d47478702b5b44419937d35e7085
Shigeki Ohtsu <ohtsu@ohtsu.org>
2016-11-30 20:02:32 +0900

    update

--- run_quic.sh
+++ run_quic.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo ./quic_server --port=443 --user-data-dir=/tmp/chrome-profile --quic_in_memory_cache_dir=./cache --certificate_file=/home/ohtsu/tmp/certs/bundle.crt  --key_file=/home/ohtsu/tmp/certs/serverkey.pkcs8
+sudo /home/ohtsu/github/proto-quic/src/out/Default/quic_server --port=443 --user-data-dir=/tmp/chrome-profile --quic_in_memory_cache_dir=./cache --certificate_file=/home/ohtsu/github/ohtsuCA/ohtsu_org.cert --key_file=/home/ohtsu/github/ohtsuCA/ohtsu_org.pkcs8
